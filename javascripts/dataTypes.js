f379b2277d228479f9cbc2a90d81ed9c562101e3
rotht2 <tyler.roth09@gmail.com>
2017-08-08 10:30:51 -0400

    starting into dataTypes...

---
+++ javascripts/dataTypes.js
@@ -0,0 +1,47 @@
+"use strict";
+/** JavaScript variables are dynamic, meaning that at anytime a variable can change its typing
+ *  Example: a variable can be defined to equal a number and later be set to equal a string
+ */
+let num = 100;
+num = "No longer a number"
+console.log(num);
+
+/** There are 7 JavaScript types:
+ *    - Number
+ *    - String
+ *    - boolean
+ *    - null
+ *    - undefined
+ *    - object
+ *    - symbol
+*/
+
+/** Number
+ *  A number serves both floating point numbers and integers
+ *  There are 2 "special" Numbers that belong to the number type.
+ *    - Infinity: Represent mathematical infinity, a number that is greater than any number
+ *    - NaN: Represents a computational error
+ *   Mathematical operations are safe in JavaScript and will not cause the script to stop with a fatal error
+ *   Worst case scenario we will see a NaN, for example if we divide a string by a number
+ */
+num = 123;
+console.log("int representation: " + num);
+num = 1.23;
+console.log("float representation: " + num);
+console.log("computational error: " + ("batman"/num));
+console.log("infinity representation: " + Infinity);
+console.log("infinity representation: " + (1/0));
+console.log("-----------------------------------------------------------------------");
+
+/** String
+ *  All strings in JavaScript must be quoted. There are 3 types of quotes:
+ *    - ""
+ *    - ''
+ *    - ``
+ *   Double quotes and single quotes are interprated the same by the compiler
+ *   Backticks grant extended functionality of the string, an example of this extended functionality is string interpolation
+ */
+let snglStr = 'This is a string'
+let dblStr = "This is a string"
+console.log(snglStr + " = " + dblStr);
+console.log(`Using interpolation this time.. ${snglStr} = ${dblStr}`)
--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,20 +1,28 @@
 // Modern JavaScript requires the following directive "use strict"
 "use script";
 
+/** In the old days of JavaScript the var keyword was used to define a variable
+ *  Now the let keyword is used to define variables
+ */
+let message = "This is an example of... "
+
+/** There are also constant (finals) that can be declared with the const command */
+const SEMICOLON = "semicolon"
+
 /** JavaScript traditionall requires a semi colon at the end of a line.
  *  Now JavaScript can insert a semicolon on the fly when they are needed
  *  But like in English this rule can be broken.. 
  *  TODO TWRR pick this up with a sample tomorrow. 
 */ 
-console.log("This is an example of a JavaScript message that does not need a semicolon");
-console.log("This is an example of a" + 
-            "JavaScript line that does require a semicolon");
+console.log( message + "JavaScript message that does not need a " + SEMICOLON);
+console.log( message + 
+            "JavaScript line that does require a " + SEMICOLON);
 
-/* The following example shows a multiline statement that requires a semicolon.
-*  The wierd exception to the rule is that JavaScript wont add a semicolon before a set of []
-*  So to see a failure remove the semicolon on the console.log line right before the loop.
-*
-*  This example shows the starts of a callback function, which is a big component of JavaScript. 
+/** The following example shows a multiline statement that requires a semicolon.
+ *  The wierd exception to the rule is that JavaScript wont add a semicolon before a set of []
+ *  So to see a failure remove the semicolon on the console.log line right before the loop.
+ *
+ *  This example shows the starts of a callback function, which is a big component of JavaScript. 
 */ 
 console.log("This should be fine");
 [1, 2].forEach(function(){
